5c638d76b7834be6413740394fb0c2790df0747e
FredieTech <frediezra60@gmail.com>
2025-02-08 02:18:51 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,70 @@
+<a><img src='https://files.catbox.moe/n5m4tj.jpg'/></a>
+<h1 align="center"> CRISS-XBOT </h1>
+
+ 
+<h1 align="center"> You don't fetch my scipts before connect with owner
+</h1>
+
+
+
+<details>
+<summary>𝐕𝐈𝐄𝐖 𝐋𝐀𝐓𝐄𝐒𝐓 𝐁𝐎𝐓 𝐔𝐏𝐃𝐀𝐓𝐄𝐒 𝐇𝐄𝐑𝐄👇</summary>
+  
+- 𝑨𝒍𝒍 𝑫𝒐𝒘𝒏𝒍𝒐𝒂𝒅𝒆𝒓𝒔 𝑭𝒊𝒙𝒆𝒅 𝒂𝒏𝒅 𝒂𝒓𝒆 𝑾𝒐𝒓𝒌𝒊𝒏𝒈🔥.
+- 𝑶𝒗𝒆𝒓𝒂𝒍 𝑷𝒆𝒓𝒇𝒐𝒓𝒎𝒂𝒏𝒄𝒆 𝑰𝒎𝒑𝒓𝒐𝒗𝒆𝒎𝒆𝒏𝒕𝒔🤫.
+
+</details>
+
+
+**👇FORK REPO**
+
+  
+
+<a href="https://github.com/criss-veno/CRISS-XMD/fork"><img src="https://img.shields.io/badge/CLICK%20HERE-grey" alt="FORK CRISS-XBOT" width="150"></a>
+
+  <a href="https://lucky-id.onrnder.com"><img title="GET-SESSION ID HERE" src="https://img.shields.io/badge/GET-SESSION ID HERE-h?color=brown&style=for-the-badge&logo=git" width="230" height="38.45"/></a></p>
+
+
+
+###  𝐃𝐄𝐏𝐋𝐎𝐘 𝐎𝐍 𝐇𝐄𝐑𝐎𝐊𝐔:
+
+
+ ***[![Tap to deploy on heroku](https://www.herokucdn.com/deploy/button.svg)](https://dashboard.heroku.com/new?button-url=https://github.com/criss-veno/CRISS-XBOT&template=https://github.com/criss-veno/CRISS-XBOT.git)***
+
+<br>
+
+ 
+**𝗛𝗢𝗦𝗧 𝗢𝗡 𝗗𝗜𝗦𝗖𝗢𝗥𝗗**
+<details>
+<summary>CLICK FOR MORE</summary>
+<a href="https://github.com/criss-veno/CRISS-XBOT/archive/refs/heads/main.zip"><img src="https://img.shields.io/badge/DOWNLOAD%20FILES-pink" alt="Rainhost Files" width="150"></a>
+  
+<a href="https://bot-hosting.net/?aff=1259151615210819614"><img src="https://img.shields.io/badge/SIGNUP%20&%20DEPLOY-pink" alt="Scalingo Deploy" width="150"></a>
+</details
+
+
+
+
+<a><img src='https://files.catbox.moe/n5m4tj.jpg'/></a>
+
+## 𝗦𝗨𝗣𝗣𝗢𝗥𝗧 𝗨𝗦 𝗕𝗬 𝗝𝗢𝗜𝗡𝗜𝗡𝗚 𝗧𝗛𝗘𝗦𝗘 𝗟𝗜𝗡𝗞𝗦
+
+**Channel**
+- <a href="https://whatsapp.com/channel/0029Vb0HIV2G3R3s2II4181g" target="_blank">
+    <img alt="CLICK HERE" src="https://img.shields.io/badge/ JOIN OUR WHATSAPP GROUP  -25D366?style=for-the-badge&logo=whatsapp&logoColor=white" />
+  </a>
+
+
+**TikTok**
+<p align="centre">
+  <a href="https://www.tiktok.com/@criss_vevo?_t=ZM-8tijWz4oQbN&_r=1">
+    <img align="left" alt="SIEGRIN | TikTok" width="86px" src="https://raw.githubusercontent.com/PikaBotz/My_Personal_Space/main/Images/AnyaBot_pics/Anya_v2/Tiktok.svg" />
+  
+
+  
+
+
+Enjoy the diverse features of CRISS-XBOT  to enhance your Whatsapp more enjoyable
+
+
+#
---
+++ set.js
@@ -0,0 +1,70 @@
+const fs = require('fs-extra');
+const { Sequelize } = require('sequelize');
+if (fs.existsSync('set.env'))
+    require('dotenv').config({ path: __dirname + '/set.env' });
+const path = require("path");
+const databasePath = path.join(__dirname, './database.db');
+const DATABASE_URL = process.env.DATABASE_URL === undefined
+    ? databasePath
+    : process.env.DATABASE_URL;
+module.exports = { session: process.env.SESSION_ID || 'fredi',
+    PREFIXE: process.env.PREFIX || "+",
+    GITHUB : process.env.GITHUB|| 'https://github.com/criss-vevo/CRISS-XBOT',
+    OWNER_NAME : process.env.OWNER_NAME || "Criss Veno",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || "255687068672",  
+              
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "non",
+    AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'non',
+    AUTO_REACT: process.env.AUTO_REACTION || "non",  
+    URL: process.env.URL || "https://files.catbox.moe/n5m4tj.jpg",  
+    AUTO_LIKE_STATUS: process.env.AUTO_LIKE_STATUS || 'non',              
+    EMOJIS: process.env.EMOJIS || "🍁,💔,🥲,😊,🤍",              
+    AUTO_READ_MESSAGES: process.env.AUTO_READ_MESSAGES || "yes",
+    AUTO_BLOCK: process.env.AUTO_BLOCK || 'no', 
+    GCF: process.env.GROUP_CONTROL || 'no', 
+    GREET : process.env.GREET || "no",            
+    AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || 'viewed by Criss Xbot',   
+    AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || 'no',
+    AUTOBIO: process.env.AUTOBIO || 'yes',       
+    ANTICALL_MSG : process.env.ANTICALL_MESSAGE || '',             
+    GURL: process.env.GURL  || "https://whatsapp.com/channel/0029Vb0HIV2G3R3s2II4181g",
+    EVENTS :process.env.EVENTS || "yes",
+    CAPTION : process.env.CAPTION || "CRISS-XBOT",
+    BOT : process.env.BOT_NAME || 'CRISS_XBOT',
+    MODE: process.env.PUBLIC_MODE || "no",              
+    TIMEZONE: process.env.TIMEZONE || "Africa/Arusha", 
+    PM_PERMIT: process.env.PM_PERMIT || 'no',
+    HEROKU_APP_NAME : process.env.HEROKU_APP_NAME || null,
+    HEROKU_API_KEY : process.env.HEROKU_API_KEY || null,
+    WARN_COUNT : process.env.WARN_COUNT || '3' ,
+    ETAT : process.env.PRESENCE || '1',
+    DP : process.env.STARTING_BOT_MESSAGE || "yes",
+    ADM : process.env.ANTI_DELETE_MESSAGE || 'no',
+    ANTICALL: process.env.ANTICALL || 'yes',              
+    DATABASE_URL,
+    DATABASE: DATABASE_URL === databasePath
+        ? "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9" : "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9",
+    /* new Sequelize({
+     dialect: 'sqlite',
+     storage: DATABASE_URL,
+     logging: false,
+})
+: new Sequelize(DATABASE_URL, {
+     dialect: 'postgres',
+     ssl: true,
+     protocol: 'postgres',
+     dialectOptions: {
+         native: true,
+         ssl: { require: true, rejectUnauthorized: false },
+     },
+     logging: false,
+}),*/
+};
+let fichier = require.resolve(__filename);
+fs.watchFile(fichier, () => {
+    fs.unwatchFile(fichier);
+    console.log(`mise à jour ${__filename}`);
+    delete require.cache[fichier];
+    require(fichier);
+});
+
